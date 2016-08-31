2c0a1d980fc04accfb996b900a02282e9d95310e
player0k <player0k@mail.ru>
2016-08-31 22:11:59 +0300

    Update hooks.php

--- hooks.php
+++ hooks.php
@@ -6,16 +6,16 @@
  * Call add_post_type_support('{post-type}', 'offshore-ads-manager')
  * for post types checked on Offshore ADS Manager Settings page.
  */
-function _action_fw_ext_offshore_ads_manager_add_support() {
-	$feature_name = fw_ext('offshore-ads-manager')->get_supports_feature_name();
+function _action_fw_ext_banner_manager_add_support() {
+	$feature_name = fw_ext('banner-manager')->get_supports_feature_name();
 
 	foreach (
-		array_keys(fw_get_db_ext_settings_option('offshore-ads-manager', 'post_types'))
+		array_keys(fw_get_db_ext_settings_option('banner-manager', 'post_types'))
 		as $slug
 	) {
 		add_post_type_support($slug, $feature_name);
 	}
 }
-add_action( 'init', '_action_fw_ext_offshore_ads_manager_add_support',
+add_action( 'init', '_action_fw_ext_banner_manager_add_support',
 	9999
 );
